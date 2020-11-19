481e6ea3e2289c2c9c33d8be6ced9118d74e2ce2
Sourish Mukherjee <mukherjee.sourish09@gmail.com>
2020-11-19 10:47:53 +0530

    Removed Commen from NotificationManager

--- lib/Screens/toDoScreen/backend/notificationManager.dart
+++ lib/Screens/toDoScreen/backend/notificationManager.dart
@@ -19,7 +19,6 @@ class NotificationManager {
   }
 
   void initNotifications() {
-    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     var initializationSettingsAndroid =
         new AndroidInitializationSettings('app_icon');
     var initializationSettingsIOS = IOSInitializationSettings(
