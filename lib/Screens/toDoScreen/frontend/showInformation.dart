9311c273d3ebca9c631f0bf0274c19cff8efefdb
Sourish Mukherjee <mukherjee.sourish09@gmail.com>
2020-11-19 10:02:37 +0530

    Remove DoubleTap Code

--- lib/Screens/toDoScreen/frontend/showInformation.dart
+++ lib/Screens/toDoScreen/frontend/showInformation.dart
@@ -16,67 +16,6 @@ class ShowInformation {
         });
   }
 
-  void showOnDoubleTap(String email, BuildContext context, DocumentSnapshot e,
-      AsyncSnapshot snapshot,
-      {NotificationManager notificationManager}) {
-    int ind = e['index'];
-    Widget deletebutton = FlatButton(
-        child: Text(
-          "Delete",
-          style: TextStyle(
-            color: Colors.teal,
-          ),
-        ),
-        onPressed: () {
-          FirebaseFirestore.instance.runTransaction((transaction) async {
-            transaction.delete(e.reference);
-            Fluttertoast.showToast(msg: "Task has been deleted!");
-            Navigator.of(context, rootNavigator: true).pop();
-          }).then((value) => MakeChanges(
-                  email: email, notificationManager: notificationManager)
-              .updateData(ind, snapshot.data.documents.length - 1, false));
-          notificationManager.removeReminder(e['notificationID']);
-        });
-    Widget cancelbutton = FlatButton(
-      child: Text(
-        "Cancel",
-        style: TextStyle(
-          color: Colors.teal,
-        ),
-      ),
-      onPressed: () => Navigator.of(context, rootNavigator: true).pop(),
-    );
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(20.0),
-          ),
-          backgroundColor: Colors.grey[850],
-          title: Text(
-            e['Title'],
-            style: TextStyle(
-              fontSize: 22.0,
-              color: Colors.white,
-            ),
-          ),
-          content: Text(
-            "Are you sure you want to delete?",
-            style: TextStyle(
-              fontSize: 18.0,
-              color: Colors.white,
-            ),
-          ),
-          actions: [
-            cancelbutton,
-            deletebutton,
-          ],
-        );
-      },
-    );
-  }
-
   void onSignOut(
       {BuildContext context, NotificationManager notificationManager}) {
     Widget signButton = FlatButton(
